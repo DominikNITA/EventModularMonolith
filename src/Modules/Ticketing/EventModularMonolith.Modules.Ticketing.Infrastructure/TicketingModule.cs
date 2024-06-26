9a62c32c96ed7cfb8a5332472b473a55d0ddab4c
DominikNITA <36695471+DominikNITA@users.noreply.github.com>
2024-06-27 01:18:05 +0200

    WIP - Add ticketing module

---
+++ src/Modules/Ticketing/EventModularMonolith.Modules.Ticketing.Infrastructure/TicketingModule.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection.Metadata;
+using EventModularMonolith.Modules.Ticketing.Application.Carts;
+using EventModularMonolith.Shared.Presentation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventModularMonolith.Modules.Ticketing.Infrastructure;
+
+public static class TicketingModule
+{
+   public static IServiceCollection AddTicketingModule(
+      this IServiceCollection services,
+      IConfiguration configuration)
+   {
+      services.AddInfrastructure(configuration);
+
+      services.AddEndpoints(Presentation.AssemblyReference.Assembly);
+
+      return services;
+   }
+
+#pragma warning disable S1172 // Unused method parameters should be removed
+#pragma warning disable IDE0060
+   private static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+#pragma warning restore IDE0060
+#pragma warning restore S1172 // Unused method parameters should be removed
+   {
+      services.AddSingleton<CartService>();
+   }
+}
